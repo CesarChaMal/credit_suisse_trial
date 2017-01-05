6c1409bb1f302764d45aa028f4518d3119434547
CesarChaMal <ces_ch@hotmail.com>
2017-01-05 03:00:39 -0600

    linux script updated

--- run-app.sh
+++ run-app.sh
@@ -5,7 +5,8 @@
 #
 
 function cleanup() {
-    kill ${SERVER_PID} ${CLIENT_PID}
+#    kill ${SERVER_PID} ${CLIENT_PID}
+    kill -9 ${SERVER_PID}
 }
 
 trap cleanup EXIT
@@ -13,7 +14,7 @@ trap cleanup EXIT
 # mvn -DskipTests compile
 mvn compile test
 
-mvn jetty:run & SERVER_PID=$$
+mvn jetty:run & SERVER_PID=$!
 
 while ! nc localhost 8080 > /dev/null 2>&1 < /dev/null; do
     echo "$(date) - waiting for server at localhost:8080..."
@@ -21,9 +22,9 @@ while ! nc localhost 8080 > /dev/null 2>&1 < /dev/null; do
     sleep 1  
 done
 
-#while ! nc localhost 8080 > /dev/null 2>&1 < /dev/null; do
-#	curl -i -X GET -H Accept:application/json "http://localhost:8080/credit_suisse_trial/"
-#   sleep 5  
-#done
-#CLIENT_PID=$$
-#cleanup
+while ! nc localhost 8080 > /dev/null 2>&1 < /dev/null; do
+	curl -i -X GET -H Accept:application/json "http://localhost:8080/credit_suisse_trial/"
+   sleep 5  
+done
+#CLIENT_PID=$!
+cleanup
